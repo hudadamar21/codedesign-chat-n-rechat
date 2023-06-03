a36e35d08d12358b3734047fe5663a67d4fba004
hudadamar21 <hudadamar21@gmail.com>
2023-06-03 17:01:43 +0700

    Create MainLayout.svelte

---
+++ src/layouts/MainLayout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import MainHeader from "./partials/MainHeader.svelte";
+  import MainFooter from "./partials/MainFooter.svelte";
+</script>
+
+<MainHeader/>
+<slot></slot>
+<MainFooter/>
