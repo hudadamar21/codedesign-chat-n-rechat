8c14e017c3bd570076322307f52627cd939c08fb
hudadamar21 <hudadamar21@gmail.com>
2023-06-03 17:01:26 +0700

    Create MainHeader.svelte

---
+++ src/layouts/partials/MainHeader.svelte
@@ -0,0 +1,10 @@
+<header class="max-w-screen-lg mx-auto w-full flex items-center justify-between py-10">
+  <img src="/logo.png" alt="logo">
+  <nav class="space-x-12">
+    <a href="#beranda">Beranda</a>
+    <a href="#fitur">Fitur-fitur</a>
+    <a href="#teknologi">Teknologi</a>
+    <a href="#faq">FAQ</a>
+  </nav>
+  <a href="#download" class="bg-primary text-white rounded-lg px-6 py-2 text-lg font-semibold shadow-xl shadow-primary/25">Download</a>
+</header>
