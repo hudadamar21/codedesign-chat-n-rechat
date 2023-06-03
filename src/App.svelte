9d986ba15a8c24d2d4d9def177e0946f7cb09b78
hudadamar21 <hudadamar21@gmail.com>
2023-06-03 17:02:26 +0700

    add top section to page

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import TopSection from "./components/sections/TopSection.svelte";
   import MainLayout from "./layouts/MainLayout.svelte";
 </script>
 
 <MainLayout>
-  
+  <main class="main-container">
+    <TopSection/>
+  </main>
 </MainLayout>
