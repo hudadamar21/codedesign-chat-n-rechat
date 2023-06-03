fea657993bafd586c2765f1eac882313389e718d
hudadamar21 <hudadamar21@gmail.com>
2023-06-03 15:01:48 +0700

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4A72FF',
+        secondary: '#F4F7FF',
+        darK: '#0C1B4D',
+        cream: '#FFD39F'
+      }
+    },
+  },
+  plugins: [],
+}
+
