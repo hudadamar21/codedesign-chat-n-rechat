4a6de66955dd38724fd6c77b90123a869762fed2
hudadamar21 <hudadamar21@gmail.com>
2023-06-06 11:22:11 +0700

    add to the page 🙂👍

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import DownloadSection from "./components/sections/DownloadSection.svelte";
+  import FaqSection from "./components/sections/FaqSection.svelte";
+  import FiturSection from "./components/sections/FiturSection.svelte";
+  import TeknologiSection from "./components/sections/TeknologiSection.svelte";
   import TopSection from "./components/sections/TopSection.svelte";
   import MainLayout from "./layouts/MainLayout.svelte";
 </script>
@@ -6,5 +10,9 @@
 <MainLayout>
   <main class="main-container">
     <TopSection/>
+    <FiturSection/>
+    <TeknologiSection/>
+    <DownloadSection/>
   </main>
+  <FaqSection/>
 </MainLayout>
