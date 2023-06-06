4ebc9f8e560c9d0c1bf0e6cd52fa754895d53ff1
hudadamar21 <hudadamar21@gmail.com>
2023-06-06 11:21:52 +0700

    create footer

---
+++ src/layouts/partials/MainFooter.svelte
@@ -0,0 +1,43 @@
+<footer class="main-container mt-32">
+  <div class="bg-primary/5 p-16 grid grid-cols-4 gap-10 rounded-t-2xl">
+    <div class="space-y-4 col-span-2">
+      <div class="flex items-center gap-3">
+        <img src="/logo.png" alt="">
+        <h2>Chat N’ Rechat</h2>
+      </div>
+      <p class="text-dark/60 pr-10">
+        Sebenernya banyak banget aplikasi chatting yang lebih bagus dari ini, maka dari itu lebih baik pake yang lain aja deh ya daripada pake aplikasi yang ini.
+      </p>
+      <div class="text-sm text-dark/60 font-medium tracking-widest space-y-3">
+        <div>COPYRIGHT (C) 2021. </div>
+        <div>DESIGN BY NAUVAL</div>
+        <div>DEVELOP BY HUDA</div>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-lg font-medium mb-4">Sitemap</h2>
+      <div class="text-dark/60 space-y-3">
+        <a href="#beranda" class="block font-semibold hover:underline">Beranda</a>
+        <a href="#fitur" class="block font-semibold hover:underline">Fitur-fitur</a>
+        <a href="#teknologi" class="block font-semibold hover:underline">Teknologi</a>
+        <a href="#faq" class="block font-semibold hover:underline">FAQ</a>
+        <a href="#download" class="block font-semibold hover:underline">Download</a>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-lg font-medium mb-4">Tetep Terhubung</h2>
+      <p class="text-dark/60">Lihat kami pansos di sosial media berikut:</p>
+      <div class="flex items-center gap-5 mt-5">
+        <a href="#instagram" class="h-10 rounded-lg aspect-square flex items-center justify-center bg-dark/10">
+          <img src="/images/instagram.svg" alt="instagram">
+        </a>
+        <a href="#facebook" class="h-10 rounded-lg aspect-square flex items-center justify-center bg-dark/10">
+          <img src="/images/facebook.svg" alt="facebook">
+        </a>
+        <a href="#twitter" class="h-10 rounded-lg aspect-square flex items-center justify-center bg-dark/10">
+          <img src="/images/twitter.svg" alt="twitter">
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
