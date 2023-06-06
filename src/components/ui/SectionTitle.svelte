43c840efe5a2db1931d1556a4c074ab6e7da4336
hudadamar21 <hudadamar21@gmail.com>
2023-06-06 11:21:45 +0700

    create section title component

---
+++ src/components/ui/SectionTitle.svelte
@@ -0,0 +1,11 @@
+<script lang="typescript">
+export let badge;
+export let title;
+export let description;
+</script>
+
+<div class="text-sm font-semibold mb-4 tracking-widest">{badge}</div>
+<h2 class="text-[28px] leading-[42px] mb-3">{title}</h2>
+<p class="text-dark/60 leading-[26px]">
+  {description}
+</p>
