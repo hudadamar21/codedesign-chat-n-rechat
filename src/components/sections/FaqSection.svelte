9ea431162a685b8f411235414c44142f3a0b9dd9
hudadamar21 <hudadamar21@gmail.com>
2023-06-06 11:21:34 +0700

    create faq section

---
+++ src/components/sections/FaqSection.svelte
@@ -0,0 +1,13 @@
+<script>
+  import SectionTitle from "../ui/SectionTitle.svelte";
+
+</script>
+<section>
+  <div id="faq" class="text-center w-[30%] mx-auto">
+    <SectionTitle
+      badge="BANYAK YANG NANYA"
+      title="Pertanyaan Yang Sering Ditanyain"
+      description="Sebenernya pertanyaan-pertanyaan ini kagak ada yang nanya sih, tapi ya ditaruh sini aja siapa tau lo mau baca ‘kan."
+    />
+  </div>
+</section>
