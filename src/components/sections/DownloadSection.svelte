87dc82f009a1f5eafb31b949a81e7d34c72e7873
hudadamar21 <hudadamar21@gmail.com>
2023-06-06 11:21:24 +0700

    create download section

---
+++ src/components/sections/DownloadSection.svelte
@@ -0,0 +1,22 @@
+<section id="download" class="bg-primary grid grid-cols-2 justify-center items-center my-32 rounded-3xl shadow-2xl shadow-primary/25">
+  <div class="text-white p-20">
+    <div class="text-sm font-semibold mb-4 tracking-widest">DOWNLOAD APLIKASINYA</div>
+    <h2 class="text-[28px] leading-[42px] mb-3">Download Aplikasi Chat n’ Rechat Di Play Store, App Store, atau Symbian Store</h2>
+    <ul class="opacity-60 leading-[26px] list-disc list-inside">
+      <li>Android Version: Mojave+</li>
+      <li>iOS Version: Lolipop+</li>
+      <li>Symbian: 20.04+</li>
+    </ul>
+    <div class="flex items-center gap-5 mt-12">
+      <a href="#playstore">
+        <img src="/images/download-playstore.png" alt="download playstore">
+      </a>
+      <a href="#appstore">
+        <img src="/images/download-appstore.png" alt="download appstore">
+      </a>
+    </div>
+  </div>
+  <div class="justify-self-center"> 
+    <img src="/images/download.png" alt="download">
+  </div>
+</section>
