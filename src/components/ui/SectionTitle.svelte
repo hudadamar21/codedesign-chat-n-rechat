f9c6ea5bc22ea67b9facb66e9693bcea98690a25
hudadamar21 <hudadamar21@gmail.com>
2023-06-10 14:54:05 +0700

    Update SectionTitle.svelte

--- src/components/ui/SectionTitle.svelte
+++ src/components/ui/SectionTitle.svelte
@@ -4,8 +4,8 @@ export let title;
 export let description;
 </script>
 
-<div class="text-xs md:text-sm font-semibold mb-4 tracking-widest">{badge}</div>
-<h2 class="text-xl md:text-2xl lg:text-[28px] font-medium leading-8 md:leading-[42px] mb-3">{title}</h2>
+<div class="font-inter text-xs md:text-sm font-semibold mb-4 tracking-widest">{badge}</div>
+<h2 class="text-xl md:text-2xl lg:text-[28px] leading-8 md:leading-[42px] mb-3">{title}</h2>
 <p class="text-sm md:text-base text-dark/60 leading-[26px]">
   {description}
 </p>
