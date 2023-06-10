20070e7df0e320248fbbc8179d97a8872e2e7518
hudadamar21 <hudadamar21@gmail.com>
2023-06-10 14:54:07 +0700

    Create Faq.svelte

---
+++ src/components/ui/Faq.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { onMount } from "svelte";
+  import IconArrowDown from "../icon/IconArrowDown.svelte";
+
+  export let faq;
+  let isOpen = false;
+  let answer;
+
+  let anwerHeight;
+  onMount(() => {
+    anwerHeight = answer.scrollHeight;
+    window.addEventListener("resize", () => {
+      anwerHeight = answer.scrollHeight;
+    });
+  });
+</script>
+
+<div
+  class={`transition-all duration-300 ${
+    isOpen ? "bg-primary shadow-xl shadow-primary/20 rounded-xl text-white mb-3" : ""
+  }`}
+>
+  <button
+    on:click={() => (isOpen = !isOpen)}
+    class={`flex items-center justify-between gap-5 border-b p-5 w-full transition-[border-color] duration-300 ${
+      isOpen ? "border-white/20" : ""
+    }`}
+  >
+    <h2 class="text-base md:text-lg text-left font-medium">{faq.question}</h2>
+    <div>
+      <IconArrowDown />
+    </div>
+  </button>
+  <div
+    style={`max-height: ${isOpen ? anwerHeight + "px" : "0px"}`}
+    class="transition-[max-height] duration-300 overflow-hidden"
+  >
+    <p bind:this={answer} class="p-5">
+      {faq.answer}
+    </p>
+  </div>
+</div>
