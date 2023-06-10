13dd0f0887f18c336f84d4730973afb936555bf1
hudadamar21 <hudadamar21@gmail.com>
2023-06-10 11:24:51 +0700

    responsive top section

--- src/components/sections/TopSection.svelte
+++ src/components/sections/TopSection.svelte
@@ -2,54 +2,65 @@
   import IconArrowRight from "../icon/IconArrowRight.svelte";
   import IconDownload from "../icon/IconDownload.svelte";
   import IconUser from "../icon/IconUser.svelte";
-
 </script>
-<section id="top" class="grid grid-cols-2 gap-10 mt-20">
-  <div>
-    <div class="text-primary text-sm font-semibold mb-4 tracking-widest">
-      CHAT N’ RECHAT
-    </div>
-    <h1 class="text-4xl mb-6">
+
+<section id="top" class="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10 md:mt-20">
+  <div class="order-2 md:order-1 px-3">
+    <div class="text-primary text-sm font-semibold mb-4 tracking-widest">CHAT N’ RECHAT</div>
+    <h1 class="text-2xl md:text-3xl lg:text-4xl mb-6">
       Sebuah aplikasi chatting yang bisa bikin kamu lupa diri, tapi tidak lupa jasa pahlawan!
     </h1>
-    <p class="text-lg text-dark/60 mb-6">
-      Sebenernya banyak banget aplikasi chatting yang lebih bagus dari ini, maka dari itu lebih baik pake yang lain aja deh ya daripada pake aplikasi yang ini.
+    <p class="text-base md:text-lg text-dark/60 mb-6">
+      Sebenernya banyak banget aplikasi chatting yang lebih bagus dari ini, maka dari itu lebih baik
+      pake yang lain aja deh ya daripada pake aplikasi yang ini.
     </p>
     <div class="flex items-center gap-5 mb-14">
-      <a href="#register" class="flex items-center justify-center gap-3 bg-primary rounded-lg px-[45px] py-[15px] text-lg font-semibold text-white shadow-xl shadow-primary/25">
+      <a
+        href="#register"
+        class="flex items-center justify-center gap-3 bg-primary rounded-lg px-8 md:px-[45px] py-3 md:py-[15px] text-base md:text-lg font-semibold text-white shadow-xl shadow-primary/25"
+      >
         Register
-        <IconArrowRight/>
+        <IconArrowRight />
       </a>
-      <a href="#learn" class="flex items-center justify-center gap-3 bg-primary rounded-lg px-[45px] py-[15px] text-lg font-semibold text-primary bg-primary/5">
+      <a
+        href="#learn"
+        class="flex items-center justify-center gap-3 bg-primary rounded-lg px-8 md:px-[45px] py-3 md:py-[15px] text-base md:text-lg font-semibold text-primary bg-primary/5"
+      >
         Learn More
       </a>
     </div>
-    <div class="flex itemsc-center gap-10 divide-x">
-      <div class="flex items-start gap-8">
-        <i class="mt-1"><IconDownload/></i>
+    <div class="flex items-center gap-5 md:gap-10 divide-x">
+      <div class="flex items-start gap-3 md:gap-8">
+        <i class="mt-1"><IconDownload /></i>
         <div>
-          <small class="text-xs text-dark/60">TOTAL DON’TLOUD</small>
-          <div class="text-2xl font-semibold">1,501,234</div>
+          <small class="text-[10px] md:text-xs text-dark/60">TOTAL DON’TLOUD</small>
+          <div class="text-xl md:text-2xl font-semibold">1,501,234</div>
         </div>
       </div>
-      <div class="flex items-start gap-8 pl-10">
-        <i class="mt-1"><IconUser/></i>
+      <div class="flex items-start gap-3 md:gap-8 pl-5 md:pl-10">
+        <i class="mt-1"><IconUser /></i>
         <div>
-          <small class="text-xs text-dark/60">TOTAL USERS</small>
-          <div class="text-2xl font-semibold">1,318,829</div>
+          <small class="text-[10px] md:text-xs text-dark/60">TOTAL USERS</small>
+          <div class="text-xl md:text-2xl font-semibold">1,318,829</div>
         </div>
       </div>
     </div>
   </div>
   <div>
-    <img class="w-full h-full scale-[1.15]" src="/images/top-image.svg" alt="top">
+    <img
+      class="w-2/3 md:w-full mx-auto h-full scale-[1.15]"
+      src="/images/top-image.svg"
+      alt="top"
+    />
   </div>
 </section>
 
-<section class="bg-primary/5 w-full rounded-2xl py-14 flex items-center justify-center gap-10 mt-32">
-  <img src="/images/sponsors/cnn.png" alt="cnn">
-  <img src="/images/sponsors/tirto.png" alt="tirto">
-  <img src="/images/sponsors/forbes.png" alt="forbes">
-  <img src="/images/sponsors/google.png" alt="google">
-  <img src="/images/sponsors/the-new-york-times.png" alt="the new york times">
+<section
+  class="bg-primary/5 w-full md:rounded-2xl py-14 flex flex-col md:flex-row items-center justify-center gap-14 md:gap-10 mt-20 md:mt-32"
+>
+  <img src="/images/sponsors/cnn.png" alt="cnn" />
+  <img src="/images/sponsors/tirto.png" alt="tirto" />
+  <img src="/images/sponsors/forbes.png" alt="forbes" />
+  <img src="/images/sponsors/google.png" alt="google" />
+  <img src="/images/sponsors/the-new-york-times.png" alt="the new york times" />
 </section>
