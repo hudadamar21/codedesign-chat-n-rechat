6a2a58bce473de457c611b6a999834a3069eefef
hudadamar21 <hudadamar21@gmail.com>
2023-06-10 11:25:39 +0700

    responsive main footer

--- src/layouts/partials/MainFooter.svelte
+++ src/layouts/partials/MainFooter.svelte
@@ -1,22 +1,22 @@
-<footer class="main-container mt-32">
-  <div class="bg-primary/5 p-16 grid grid-cols-4 gap-10 rounded-t-2xl">
-    <div class="space-y-4 col-span-2">
+<footer class="main-container mt-20 md:mt-32">
+  <div class="bg-primary/5 py-16 px-3 md:p-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 rounded-t-2xl">
+    <div class="space-y-4 md:col-span-2">
       <div class="flex items-center gap-3">
         <img src="/logo.png" alt="">
         <h2>Chat N’ Rechat</h2>
       </div>
-      <p class="text-dark/60 pr-10">
+      <p class="text-sm md:text-base text-dark/60 pr-10">
         Sebenernya banyak banget aplikasi chatting yang lebih bagus dari ini, maka dari itu lebih baik pake yang lain aja deh ya daripada pake aplikasi yang ini.
       </p>
-      <div class="text-sm text-dark/60 font-medium tracking-widest space-y-3">
+      <div class="text-xs md:text-sm text-dark/60 font-medium tracking-widest space-y-3">
         <div>COPYRIGHT (C) 2021. </div>
         <div>DESIGN BY NAUVAL</div>
         <div>DEVELOP BY HUDA</div>
       </div>
     </div>
     <div>
-      <h2 class="text-lg font-medium mb-4">Sitemap</h2>
-      <div class="text-dark/60 space-y-3">
+      <h2 class="text-base md:text-lg font-medium mb-3 md:mb-4">Sitemap</h2>
+      <div class="text-dark/60 space-y-3 text-sm md:text-base">
         <a href="#beranda" class="block font-semibold hover:underline">Beranda</a>
         <a href="#fitur" class="block font-semibold hover:underline">Fitur-fitur</a>
         <a href="#teknologi" class="block font-semibold hover:underline">Teknologi</a>
@@ -25,17 +25,17 @@
       </div>
     </div>
     <div>
-      <h2 class="text-lg font-medium mb-4">Tetep Terhubung</h2>
-      <p class="text-dark/60">Lihat kami pansos di sosial media berikut:</p>
+      <h2 class="text-base md:text-lg font-medium mb-3 md:mb-4">Tetep Terhubung</h2>
+      <p class="text-sm md:text-base text-dark/60">Lihat kami pansos di sosial media berikut:</p>
       <div class="flex items-center gap-5 mt-5">
         <a href="#instagram" class="h-10 rounded-lg aspect-square flex items-center justify-center bg-dark/10">
-          <img src="/images/instagram.svg" alt="instagram">
+          <img class="h-4 md:h-8" src="/images/instagram.svg" alt="instagram">
         </a>
         <a href="#facebook" class="h-10 rounded-lg aspect-square flex items-center justify-center bg-dark/10">
-          <img src="/images/facebook.svg" alt="facebook">
+          <img class="h-4 md:h-8" src="/images/facebook.svg" alt="facebook">
         </a>
         <a href="#twitter" class="h-10 rounded-lg aspect-square flex items-center justify-center bg-dark/10">
-          <img src="/images/twitter.svg" alt="twitter">
+          <img class="h-4 md:h-8" src="/images/twitter.svg" alt="twitter">
         </a>
       </div>
     </div>
